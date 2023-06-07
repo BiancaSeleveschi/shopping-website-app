f0c182c7eeb87b2787437c64ce606d2cfa5f878d
BiancaSeleveschi <bianca.seleveschi@gmail.com>
2023-06-07 11:00:59 +0300

    Add VUE_APP to env vars

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -9,12 +9,12 @@ import firebase from "firebase";
 
 
 const firebaseConfig = {
-    apiKey: "process.env.FIREBASE_API_KEY",
+    apiKey: "process.env.VUE_APP_FIREBASE_API_KEY",
     authDomain: "shopping-app1-b686e.firebaseapp.com",
     projectId: "shopping-app1-b686e",
     storageBucket: "shopping-app1-b686e.appspot.com",
     messagingSenderId: "976720044389",
-    appId: "process.env.FIREBASE_API_ID",
+    appId: "process.env.VUE_APP_FIREBASE_API_ID",
     measurementId: "G-GH1415LYBG"
 };
 const firebase1 = firebase.initializeApp(firebaseConfig)
