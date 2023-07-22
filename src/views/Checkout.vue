22f506561c157b5b82284fdf136112fbb3cb1ea7
BiancaSeleveschi <bianca.seleveschi@gmail.com>
2023-07-22 19:38:06 +0300

    change url to return_url in data - Checkout.vue

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -192,7 +192,7 @@ export default {
         clientSecret: this.clientSecret
       },
       confirmParams: {
-        return_url: 'http://localhost:8080/order/confirmation',
+        return_url: 'https://shopping-app-meduzza.netlify.app/order/confirmation',
       },
     };
   },
